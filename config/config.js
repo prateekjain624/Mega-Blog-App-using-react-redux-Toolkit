9203dd35109674000cb2f290b02a1632bf3feb6e
Prateek jain <prateek.jn624@gmail.com>
2025-01-07 22:55:40 +0530

    setup the project

---
+++ config/config.js
@@ -0,0 +1,9 @@
+const config = {
+  appwriteUrl: String("https://cloud.appwrite.io/v1"),
+  appwriteProjectId: String("677d48b100288d0c4998"),
+  appwriteDatabaseId: String("677d496c000bb0def54e"),
+  appwriteCollectionId: String("677d49a1001e725623bc"),
+  appwriteBucketId: String("677d5f9200242b85fcb7"),
+};
+
+export default config;
